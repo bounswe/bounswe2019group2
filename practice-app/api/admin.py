69335d9ac40132fbe1f8854e8345e01e212152ed
mertyg <mertyuksekgonul@gmail.com>
2019-04-25 18:58:30 +0300

    implemented models

--- practice-app/api/admin.py
+++ practice-app/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Parity, Equipment, ManualInvestment
 
 # Register your models here.
+admin.site.register(Equipment)
+admin.site.register(ManualInvestment)
+admin.site.register(Parity)
