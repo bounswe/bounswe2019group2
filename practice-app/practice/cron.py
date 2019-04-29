0b6ec649a2d8cf7c0759168ca588f9f0d7b4a8f9
fatih-iver <fatih.iver@boun.edu.tr>
2019-04-29 12:56:29 +0300

    format according to pep8

--- practice-app/practice/cron.py
+++ practice-app/practice/cron.py
@@ -3,6 +3,7 @@ from django_cron import CronJobBase, Schedule
 from django.utils import timezone
 from api.models import Equipment, Parity
 
+
 class GetExchangeRates(CronJobBase):
     RUN_EVERY_MINS = 5
 
@@ -32,17 +33,14 @@ class GetExchangeRates(CronJobBase):
     def do(self):
 
         for symbol, name in zip(GetExchangeRates.symbols, GetExchangeRates.names):
-            e = Equipment(symbol=symbol, name=name, category='currency')
-            try:
-                e.save()
-            except:
-                continue
+            if not Equipment.objects.filter(symbol=symbol, name=name, category='currency').exists():
+                Equipment(symbol=symbol, name=name, category='currency').save()
 
         for base_symbol in GetExchangeRates.symbols:
             for target_symbol in GetExchangeRates.symbols:
                 if base_symbol != target_symbol:
-
-                    response = requests.get("https://api.exchangeratesapi.io/latest?symbols={},{}".format(base_symbol, target_symbol))
+                    response = requests.get(
+                        "https://api.exchangeratesapi.io/latest?symbols={},{}".format(base_symbol, target_symbol))
                     response_as_json = response.json()
 
                     base_equipment = Equipment.objects.get(symbol=base_symbol)
@@ -50,6 +48,7 @@ class GetExchangeRates(CronJobBase):
                     ratio = response_as_json['rates'][target_symbol] / response_as_json['rates'][base_symbol]
                     dt = timezone.now()
 
-                    p = Parity(base_equipment=base_equipment, target_equipment=target_equipment, ratio=ratio, date=dt)
-                    p.save()
-
+                    if not Parity.objects.filter(base_equipment=base_equipment, target_equipment=target_equipment,
+                                                 ratio=ratio, date=dt).exists():
+                        Parity(base_equipment=base_equipment, target_equipment=target_equipment, ratio=ratio,
+                               date=dt).save()
