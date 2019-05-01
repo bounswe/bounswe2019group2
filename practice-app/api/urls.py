0c636d76cce55c2905607077034554a4d8b032d6
Adil Numan Celik <adil.celik@boun.edu.tr>
2019-05-01 19:56:59 +0300

    Implement list investments api

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('helloworld/', views.HelloWorldView.as_view(), name='helloworld'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('investments/', views.InvestmentsView.as_view(), name='investments'),
 ]
--- practice-app/api/views.py
+++ practice-app/api/views.py
@@ -1,9 +1,11 @@
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status as st
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers as ls
+
 
 class HelloWorldView(APIView):
     def get(self, request):
@@ -18,10 +20,34 @@ class HelloWorldView(APIView):
             'message': f'Hello, {name}.'
         })
 
+
 class LoginAPIView(APIView):
     def post(self, *args, **kwargs):
         data = self.request.data
         serializer = ls.LoginSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=st.HTTP_200_OK)
+
+
+class InvestmentsView(APIView):
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        investments = request.user.manualinvestment_set.all()
+
+        response = {
+            'investments': []
+        }
+
+        for investment in investments:
+            response['investments'].append({
+                'base_symbol': investment.base_equipment.symbol,
+                'target_symbol': investment.target_equipment.symbol,
+                'base_amount': investment.base_amount,
+                'target_amount': investment.target_amount,
+                'date': investment.date
+            })
+
+        return Response(response)
 
