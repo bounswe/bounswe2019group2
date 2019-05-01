051f6740a6442339f83fef5d26cc2797f4511801
mcanbayraktar <bayraktarmcan@gmail.com>
2019-05-01 17:59:22 +0300

    Uptodate Parity is implemented.

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.HelloWorldView.as_view(), name='helloworld'),
+    path('helloworld/', views.HelloWorldView.as_view(), name='helloworld'), path('UptodateParity/', views.UptodateParityView.as_view(), name='UptodateParity'),
 ]
--- practice-app/api/views.py
+++ practice-app/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from django.utils import timezone
+from api.models import Equipment, Parity
+from datetime import datetime
 
 class HelloWorldView(APIView):
     def get(self, request):
@@ -14,3 +16,21 @@ class HelloWorldView(APIView):
         return Response({
             'message': f'Hello, {name}.'
         })
+
+class UptodateParityView(APIView):
+	def get(self, request):
+		return Response({
+			'message': 'Hello, buddy.'
+		})
+
+	def post(self, request):
+		base_symbol = request.data['base_symbol']
+		target_symbol = request.data['target_symbol']
+		base_equipment = Equipment.objects.get(symbol=base_symbol)
+		target_equipment = Equipment.objects.get(symbol=target_symbol)
+		filtered = Parity.objects.filter(base_equipment = base_equipment, target_equipment = target_equipment)
+		return Response({
+			'Base Equipment': filtered[0].base_equipment.symbol,
+			'Target Equipment': filtered[0].target_equipment.symbol,
+			'Ratio': filtered[0].ratio
+		})
