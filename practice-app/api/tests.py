2a4466d33533f885460efe206830f10698196713
Dilruba Köse <dilrubakose@gmail.com>
2019-05-01 17:24:24 +0300

    implement test for registration

--- practice-app/api/tests.py
+++ practice-app/api/tests.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.contrib.auth.models import User
 
 
 class HelloWorldTestCase(APITestCase):
@@ -16,3 +17,45 @@ class HelloWorldTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.assertEqual(response.data, {'message': 'Hello, John Doe.'})
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_user_create(self):
+        response = self.client.put('/register/', data={'username': 'newuser', 'password': '1234qazx',
+                                                       'email': 'yeni@email.com'})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(response.data, {'message': 'User newuser is registered'})
+
+
+class InvalidRegisterTestCase(APITestCase):
+    def setUp(self):
+        user = User(username='newuser', email='yeni@email.com')
+        user.set_password('1234qazx')
+        user.save()
+
+    def test_user_same(self):
+        response = self.client.put('/register/', data={'username': 'newuser', 'password': '1234qazx',
+                                                        'email': 'yeni@email.com'})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(response.data, {'message': 'Email or username is invalid'})
+
+    def test_same_email(self):
+        response = self.client.put('/register/', data={'username': 'invester', 'password': '1234qazx',
+                                                        'email': 'yeni@email.com'})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(response.data, {'message': 'Email or username is invalid'})
+
+    def test_same_username(self):
+        response = self.client.put('/register/', data={'username': 'newuser', 'password': '1234qazx',
+                                                        'email': 'yenibiri@email.com'})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(response.data, {'message': 'Email or username is invalid'})
