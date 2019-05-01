a85942bef6fb70d5a3ee59fcca83549f7cf17089
mertyg <mertyuksekgonul@gmail.com>
2019-05-01 17:56:31 +0300

    implemented total profit calculation

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('helloworld/', views.HelloWorldView.as_view(), name='helloworld'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('investments/total_profit', views.TotalProfitAPIView.as_view(), name='total profit'),
 ]
