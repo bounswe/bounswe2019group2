9b1ea36763f82e3e805dec03cba3cae3a4e998a7
Yunus Emre Inci <yemreinci0@gmail.com>
2019-05-03 18:39:05 +0300

    small fix

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('helloworld/', views.HelloWorldView.as_view(), name='helloworld'),
     path('register/', views.RegisterView.as_view(), name='register'),
-=========
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('investments/total_profit', views.TotalProfitAPIView.as_view(), name='total profit'),
 ]
