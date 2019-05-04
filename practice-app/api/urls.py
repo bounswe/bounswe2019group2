4d8c874cf9fa8d040c1cc1e398bc8dc7afb9e328
Yunus Emre Inci <yemreinci0@gmail.com>
2019-05-04 22:31:55 +0300

    small fix

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -4,9 +4,7 @@ from rest_framework.documentation import include_docs_urls
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.HelloWorldView.as_view(), name='helloworld'),
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('helloworld/', views.HelloWorldView.as_view(), name='helloworld'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('investments/total_profit', views.TotalProfitAPIView.as_view(), name='total profit'),
     path('parity/<date>/', views.ParityView.as_view(), name='parity history'),
