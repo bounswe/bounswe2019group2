5eaff6219878cd40946741b094ce5441f53a8409
Yunus Emre Inci <yemreinci0@gmail.com>
2019-10-06 19:05:36 +0300

    Implement users API ends: create (registration), update, retrieve, list

--- traiders/backend/api/urls.py
+++ traiders/backend/api/urls.py
@@ -1,2 +1,26 @@
+from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+from django.urls import path
+from django.views.generic import TemplateView
+
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+
 urlpatterns = [
+    # documentation view
+    path('docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='docs'),
+
+    # OpenAPI schema
+    path('openapi/', get_schema_view(
+        title="TrAiders",
+        description="TrAiders API Schema",
+    ), name='openapi-schema'),
+
+    # all API ends
+    *router.urls
 ]
--- traiders/backend/api/views.py
+++ traiders/backend/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserViewSet(ModelViewSet):
+    """
+    View and edit users
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
