a0a518f5e933569d2bd05884cfdbb1cda1a1ff79
fatih-iver <fatih.iver@boun.edu.tr>
2019-10-13 20:05:37 +0300

    implement token model

--- traiders/mobile/app/src/main/java/tk/traiders/models/Token.java
+++ traiders/mobile/app/src/main/java/tk/traiders/models/Token.java
@@ -1,4 +1,39 @@
 package tk.traiders.models;
 
 public class Token {
+
+    private String key;
+    private String user_id;
+
+    public Token(String key, String user_id) {
+        this.key = key;
+        this.user_id = user_id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Token setKey(String key) {
+        this.key = key;
+        return this;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public Token setUser_id(String user_id) {
+        this.user_id = user_id;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "key='" + key + '\'' +
+                ", user_id='" + user_id + '\'' +
+                '}';
+    }
+
 }
