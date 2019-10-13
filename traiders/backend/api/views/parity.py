65bbcd18d5759572e33f07be69de612c057f8f0c
mertyg <mertyuksekgonul@gmail.com>
2019-10-13 21:24:48 +0300

    adding category to parity endpoint

--- traiders/backend/api/views/parity.py
+++ traiders/backend/api/views/parity.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from ..models import Parity, Equipment
 from ..serializers import ParitySerializer
 from django.shortcuts import get_object_or_404
-
+from django.db.models import Q
 
 class ParityViewSet(ReadOnlyModelViewSet):
     serializer_class = ParitySerializer
@@ -11,12 +11,17 @@ class ParityViewSet(ReadOnlyModelViewSet):
     def get_queryset(self):
         base = self.request.query_params.get("base_equipment")
         target = self.request.query_params.get("target_equipment")
+        category = self.request.query_params.get("category")
         filters = {}
         if base is not None:
             filters["base_equipment"] = get_object_or_404(Equipment, symbol=base)
         if target is not None:
             filters["target_equipment"] = get_object_or_404(Equipment, symbol=target)
-        if base is None and target is None:
+        if base is None and target is None and category is None:
             return Parity.objects.all()
+        elif category is not None:
+            bases = Equipment.objects.filter(category=category)
+            targets = Equipment.objects.filter(category=category)
+            return Parity.objects.filter(Q(base_equipment__in=bases) | Q(target_equipment__in=targets), **filters)
         else:
             return Parity.objects.filter(**filters)
