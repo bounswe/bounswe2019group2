c2fec7e566cea7140765338b8d4b0cba6b0f1d0a
Dilruba Köse <dilrubakose@gmail.com>
2019-10-13 17:29:51 +0300

    add footer component

--- traiders/frontend/src/components/page/Page.jsx
+++ traiders/frontend/src/components/page/Page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './page.scss';
 import Navbar from '../navbar/Navbar';
+import Footer from '../footer/Footer';
 // TODO this component should updated after the implementation of Navbar and Footer components.
 
 const Page = (props) => {
@@ -11,6 +12,7 @@ const Page = (props) => {
     <div className="root-container">
       <Navbar />
       <div className="content">{children}</div>
+      <Footer />
     </div>
   );
 };
