edee4a20dc83baafb12aa386eb374935743e83b4
fatih-iver <fatih.iver@boun.edu.tr>
2019-10-13 20:05:54 +0300

    implement equipment model

--- traiders/mobile/app/src/main/java/tk/traiders/models/Equipment.java
+++ traiders/mobile/app/src/main/java/tk/traiders/models/Equipment.java
@@ -1,4 +1,42 @@
 package tk.traiders.models;
 
 public class Equipment {
+
+    private String url;
+    private String name;
+    private String category;
+    private String symbol;
+
+    public Equipment(String url, String name, String category, String symbol) {
+        this.url = url;
+        this.name = name;
+        this.category = category;
+        this.symbol = symbol;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "url='" + url + '\'' +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", symbol='" + symbol + '\'' +
+                '}';
+    }
 }
