9a3e37856fd1fcbf0d525b6b29f3286995a4c297
Dilruba Köse <dilrubakose@gmail.com>
2019-10-13 17:37:35 +0300

    fix code style

--- traiders/frontend/src/components/footer/Footer.jsx
+++ traiders/frontend/src/components/footer/Footer.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import { Icon } from 'antd'
+import React from 'react';
+import { Icon } from 'antd';
 
-import './footer.scss'
+import './footer.scss';
 
 const Footer = () => {
-    return (
-        <div className="footer-container">
-            <div className="copyright">
-                Copyright © 2019. All Rights Reserved.
-        </div>
-            <div className="icons">
-                <a href="https://www.twitter.com"><Icon type="twitter" /></a>
-                <a href="https://www.facebook.com"><Icon type="facebook" /></a>
-                <a href="https://www.instagram.com"><Icon type="instagram" /></a>
-
-
-            </div>
-
-        </div>
-
-    );
-
+  return (
+    <div className="footer-container">
+      <div className="copyright">Copyright © 2019. All Rights Reserved.</div>
+      <div className="icons">
+        <a href="https://www.twitter.com">
+          <Icon type="twitter" />
+        </a>
+        <a href="https://www.facebook.com">
+          <Icon type="facebook" />
+        </a>
+        <a href="https://www.instagram.com">
+          <Icon type="instagram" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
