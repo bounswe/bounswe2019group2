f124ceeb468bf02c1407f258275671a8ccd240f2
Harun Eren <harunerenn97@hotmail.com>
2019-10-15 14:12:35 +0300

    fix authentication

--- traiders/backend/traiders/settings.py
+++ traiders/backend/traiders/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',  # for browsable API
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # for browsable API
     ]
 }
 
