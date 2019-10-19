5efe511e18838d27e00029708f32f8b3b6192a7a
ÖzgürSolak <ozgur@Ozgur-MacBook-Pro.local>
2019-10-19 17:00:27 +0300

    profile page created

--- traiders/mobile/app/src/main/java/tk/traiders/ui/profile/ProfileFragment.java
+++ traiders/mobile/app/src/main/java/tk/traiders/ui/profile/ProfileFragment.java
@@ -11,8 +11,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 import tk.traiders.R;
+
 
 public class ProfileFragment extends Fragment {
 
@@ -20,5 +24,14 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ViewPager viewPager = view.findViewById(R.id.viewPager_profile);
+        viewPager.setAdapter(new ProfileViewAdapter(getChildFragmentManager()));
+
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout_profile);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
