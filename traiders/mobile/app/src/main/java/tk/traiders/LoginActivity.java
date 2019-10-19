c17b61166ab2b230d7f5ac747257778ebc792d99
fatih-iver <fatih.iver@boun.edu.tr>
2019-10-19 16:00:51 +0300

    implement log in & log out & authentication

--- traiders/mobile/app/src/main/java/tk/traiders/LoginActivity.java
+++ traiders/mobile/app/src/main/java/tk/traiders/LoginActivity.java
@@ -1,16 +1,45 @@
 package tk.traiders;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editText_username;
+    private EditText editText_password;
+    private RequestQueue requestQueue;
+    final private static String URL = "https://api.traiders.tk/token/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editText_username = findViewById(R.id.editText_username);
+        editText_password = findViewById(R.id.editText_password);
+
+        requestQueue = Volley.newRequestQueue(this);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -27,4 +56,55 @@ public class LoginActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void LoginClick(View view) {
+        final String username = editText_username.getText().toString();
+        final String password = editText_password.getText().toString();
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String token = jsonObject.getString("key");
+                            String user =  jsonObject.getJSONObject("user").getString("url");
+
+                            SharedPreferences sharedPreferences = getSharedPreferences("auth", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("token", token);
+                            editor.putString("user", user);
+                            editor.commit();
+                            finish();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("username", username);
+                params.put("password", password);
+
+                return params;
+            }
+        };
+
+        requestQueue.add(postRequest);
+    }
 }
