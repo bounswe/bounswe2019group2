3f2c159c1debac379d7391af0ec65dfe118aeacc
Yunus Emre Inci <yemreinci0@gmail.com>
2019-10-21 13:12:46 +0300

    Use first instead of len.

--- traiders/backend/api/views/parity.py
+++ traiders/backend/api/views/parity.py
@@ -23,8 +23,8 @@ class ParityLatestViewSet(ReadOnlyModelViewSet):
         for base in Equipment.objects.all():
             for target in Equipment.objects.all():
                 parity = Parity.objects.order_by('-date').filter(base_equipment=base,
-                                                                 target_equipment=target)
+                                                                 target_equipment=target).first()
 
-                if len(parity) != 0:
-                    parities.append(parity[0].id)
+                if parity:
+                    parities.append(parity.id)
         return Parity.objects.filter(id__in=parities)
