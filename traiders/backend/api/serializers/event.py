959ec7f6a4e8220dc0d07e01cb2f1f13defc9a86
mertyg <mertyuksekgonul@gmail.com>
2019-10-30 18:27:24 +0300

    add patch for following

--- traiders/backend/api/serializers/event.py
+++ traiders/backend/api/serializers/event.py
@@ -1,12 +1,38 @@
 from rest_framework import serializers
 from ..models import Event
 from django_countries.serializer_fields import CountryField
+from django.contrib.auth.models import AnonymousUser
+
+
+class IsFollowingField(serializers.BooleanField):
+    def get_attribute(self, instance: Event):
+        return instance.followed_by.filter(pk=self.context['request'].user.pk).exists()
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     country = CountryField(country_dict=True, read_only=True)
+    is_following = IsFollowingField()
 
     class Meta:
         model = Event
-        fields = ["url", "id", "date", "country", "calendarId", "category",
+        fields = ["is_following", "url", "id", "date", "country", "calendarId", "category",
                   "actual", "previous", "forecast", "sourceURL", "importance"]
+
+    def update(self, instance: Event, validated_data: dict):
+        is_following = validated_data.pop('is_following', None)
+
+        if isinstance(self.context['request'].user, AnonymousUser):
+            raise serializers.ValidationError('A guest user cannot follow an event. '
+                                              'Please login to perform this action.')
+
+        followers = instance.followed_by.all()
+        if is_following is True:
+            if self.context['request'].user in followers:
+                raise serializers.ValidationError('You already follow this event.')
+            instance.followed_by.add(self.context['request'].user)
+        elif is_following is False:
+            if self.context['request'].user not in followers:
+                raise serializers.ValidationError('You already do not follow this event.')
+            instance.followed_by.remove(self.context['request'].user)
+
+        return super().update(instance, validated_data)
--- traiders/backend/api/views/event.py
+++ traiders/backend/api/views/event.py
@@ -1,21 +1,17 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins
 
 from ..models import Event
 from ..serializers import EventSerializer
 from ..filters import EventFilterSet
 
 
-class EventViewSet(ReadOnlyModelViewSet):
+class EventViewSet(mixins.RetrieveModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.UpdateModelMixin,
+                   GenericViewSet):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_class = EventFilterSet
-
-
-class EventLatestViewSet(ReadOnlyModelViewSet):
-    serializer_class = EventSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = EventFilterSet
-
-    # TODO implement latest view
