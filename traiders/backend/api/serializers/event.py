8e64f88cf871cefbfd8a9c95af8d9dfd3b1af53c
mertyg <mertyuksekgonul@gmail.com>
2019-10-30 18:32:16 +0300

    handle anon

--- traiders/backend/api/serializers/event.py
+++ traiders/backend/api/serializers/event.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import AnonymousUser
 
 class IsFollowingField(serializers.BooleanField):
     def get_attribute(self, instance: Event):
+        if self.context['request'].user.is_anonymous:
+            return False
         return instance.followed_by.filter(pk=self.context['request'].user.pk).exists()
 
 
@@ -21,16 +23,18 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     def update(self, instance: Event, validated_data: dict):
         is_following = validated_data.pop('is_following', None)
 
-        if isinstance(self.context['request'].user, AnonymousUser):
-            raise serializers.ValidationError('A guest user cannot follow an event. '
-                                              'Please login to perform this action.')
-
         followers = instance.followed_by.all()
         if is_following is True:
+            if self.context['request'].user.is_anonymous:
+                raise serializers.ValidationError('A guest user cannot follow an event. '
+                                                  'Please login to perform this action.')
             if self.context['request'].user in followers:
                 raise serializers.ValidationError('You already follow this event.')
             instance.followed_by.add(self.context['request'].user)
         elif is_following is False:
+            if self.context['request'].user.is_anonymous:
+                raise serializers.ValidationError('A guest user cannot unfollow an event. '
+                                                  'Please login to perform this action.')
             if self.context['request'].user not in followers:
                 raise serializers.ValidationError('You already do not follow this event.')
             instance.followed_by.remove(self.context['request'].user)
