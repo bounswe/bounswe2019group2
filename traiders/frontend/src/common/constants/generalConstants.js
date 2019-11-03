6d4d1ad3d95fab7fdf5b7b848c2348b4d8828585
Furkan Gök <muhammet.gok@boun.edu.tr>
2019-11-03 17:52:32 +0300

    update rootReducer and add events columns

--- traiders/frontend/src/common/constants/generalConstants.js
+++ traiders/frontend/src/common/constants/generalConstants.js
@@ -2,7 +2,7 @@
 export const navbarOptions = [
   {
     name: 'SOCIAL',
-    url: '/',
+    url: '/social',
     id: 1
   },
   {
@@ -19,5 +19,33 @@ export const navbarOptions = [
     name: 'PORTFOLIO',
     url: '/',
     id: 4
+  }
+];
+
+export const eventsTableColumns = [
+  {
+    title: 'a', // Header of that column
+    dataIndex: 'A', // Which property of the data to read
+    key: 'a'
+  },
+  {
+    title: 'b', // Header of that column
+    dataIndex: 'B', // Which property of the data to read
+    key: 'b'
+  },
+  {
+    title: 'c', // Header of that column
+    dataIndex: 'C', // Which property of the data to read
+    key: 'c'
+  },
+  {
+    title: 'd', // Header of that column
+    dataIndex: 'D', // Which property of the data to read
+    key: 'd'
+  },
+  {
+    title: 'e', // Header of that column
+    dataIndex: 'E', // Which property of the data to read
+    key: 'e'
   }
 ];
