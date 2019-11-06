ef6633a9cb9abc71e90a992b3cc5087fea64f284
Yunus Emre Inci <yemreinci0@gmail.com>
2019-11-06 23:13:20 +0300

    increase symbol size

--- traiders/backend/api/models/equipment.py
+++ traiders/backend/api/models/equipment.py
@@ -11,7 +11,7 @@ class Equipment(models.Model):
                                 blank=False)
 
     symbol = models.CharField("Equipment Symbol",
-                              max_length=8,
+                              max_length=32,
                               blank=False,
                               unique=True)
 
