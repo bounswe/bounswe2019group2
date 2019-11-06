a1d3920287ed4aff142fe2a823d0269bbbddb3d2
Harun Eren <harunerenn97@hotmail.com>
2019-11-06 19:26:12 +0300

    Portfolio updated for list property

--- traiders/backend/api/models/portfolio.py
+++ traiders/backend/api/models/portfolio.py
@@ -6,5 +6,5 @@ from .parity import Parity
 class Portfolio(models.Model):
     name = models.CharField(max_length=64, blank=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
-    followed_by = models.ManyToManyField(User, related_name='followed_by', blank=True)
-    parities = models.ManyToManyField(Parity, related_name='parities', blank=True)
+    followed_by = models.ManyToManyField(User, blank=True)
+    parities = models.ManyToManyField(Parity, blank=True)
--- traiders/backend/api/views/portfolio.py
+++ traiders/backend/api/views/portfolio.py
@@ -4,22 +4,29 @@ from rest_framework import mixins
 from ..models import Portfolio
 from ..serializers import PortfolioSerializer
 from rest_framework.exceptions import PermissionDenied
-
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class PortfolioViewSet(mixins.CreateModelMixin,
                        mixins.RetrieveModelMixin,
                        mixins.UpdateModelMixin,
+                       mixins.ListModelMixin,
                        mixins.DestroyModelMixin,
                        GenericViewSet):
     serializer_class = PortfolioSerializer
     queryset = Portfolio.objects.all()
     filter_backends = [DjangoFilterBackend]
 
+    def list(self, request, *args, **kwargs):
+        if 'user' not in request.query_params:
+            return Response({
+                'user': 'Please provide a user to see his/her portfolios'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        return super().list(request, *args, **kwargs)
+
     def check_object_permissions(self, request, portfolio):
         # Another user can only retrieve; cannot update, delete, update or partial_update
         if self.action != 'retrieve' and request.user != portfolio.owner:
             raise PermissionDenied
-
-
-
