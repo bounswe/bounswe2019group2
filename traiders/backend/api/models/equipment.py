34c887d3496eb46f81a3d927f371a73dd7c0d508
Yunus Emre Inci <yemreinci0@gmail.com>
2019-11-09 16:46:45 +0300

    Make choices seen in doc, add choices to equipment category

--- traiders/backend/api/models/equipment.py
+++ traiders/backend/api/models/equipment.py
@@ -2,15 +2,20 @@ from django.db import models
 
 
 class Equipment(models.Model):
+    CATEGORY_CHOICES = (
+        ('currency', 'currency'),
+        ('crypto', 'crypto'),
+        ('stock', 'stock'),
+    )
+
     name = models.CharField("Equipment Name",
                             max_length=64,
                             blank=False)
 
-    # TODO use choice here
-    # TODO make choices seen in doc
     category = models.CharField("Equipment Category",
                                 max_length=16,
-                                blank=False)
+                                blank=False,
+                                choices=CATEGORY_CHOICES)
 
     symbol = models.CharField("Equipment Symbol",
                               max_length=32,
