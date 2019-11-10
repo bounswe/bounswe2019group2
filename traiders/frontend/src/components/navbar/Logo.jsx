7132401977d46ec0ff3a93227b9c4f9e0a2c03bc
alperenkozkan <kursat.ozkan@boun.edu.tr>
2019-11-10 15:54:28 +0300

    the logo import fixed according to the request

--- traiders/frontend/src/components/navbar/Logo.jsx
+++ traiders/frontend/src/components/navbar/Logo.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import logo from '../../common/log0.png';
 
 const Logo = () => {
-  // eslint-disable-next-line
-  const logo = require('../../common/log0.png');
   return (
     <div className="logo">
       <Link to="/">
