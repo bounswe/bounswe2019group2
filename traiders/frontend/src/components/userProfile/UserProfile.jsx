48cd425fdf5d6148c5bbda77b0101776c0a4592e
Dilruba Köse <dilrubakose@gmail.com>
2019-11-10 22:45:01 +0200

    fix

--- traiders/frontend/src/components/userProfile/UserProfile.jsx
+++ traiders/frontend/src/components/userProfile/UserProfile.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 import Page from '../page/Page';
 import UserHeader from '../userHeader/UserHeader';
 import './user-profile.scss';
-import ParityPage from '../parityPage/ParityPage';
 
 const UserProfile = (props) => {
   const datas = props;
   return (
     <Page>
       <UserHeader data={datas.user} />
-      <ParityPage base="TRY" target="EUR" />
     </Page>
   );
 };
