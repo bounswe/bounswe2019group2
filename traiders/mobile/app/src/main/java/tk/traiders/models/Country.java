e195998de983db853dfb70e396512b816d02c8f5
fatih-iver <fatih.iver@boun.edu.tr>
2019-11-10 20:21:45 +0300

    refactor filter events

---
+++ traiders/mobile/app/src/main/java/tk/traiders/models/Country.java
@@ -0,0 +1,38 @@
+package tk.traiders.models;
+
+public class Country {
+
+    String name;
+    String code;
+
+    public Country(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Country setCode(String code) {
+        this.code = code;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                '}';
+    }
+}
