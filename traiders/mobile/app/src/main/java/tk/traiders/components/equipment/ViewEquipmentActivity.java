a3edf061aec01cfffd51c75dc8f88c9289959cb7
fatih-iver <fatih.iver@boun.edu.tr>
2019-11-11 23:39:28 +0300

    add comment for equipment

--- traiders/mobile/app/src/main/java/tk/traiders/components/equipment/ViewEquipmentActivity.java
+++ traiders/mobile/app/src/main/java/tk/traiders/components/equipment/ViewEquipmentActivity.java
@@ -1,6 +1,7 @@
 package tk.traiders.components.equipment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -8,9 +9,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -18,8 +23,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import tk.traiders.LoginActivity;
+import tk.traiders.MainActivity;
 import tk.traiders.R;
 import tk.traiders.components.article.ViewArticleActivity;
 import tk.traiders.components.article.adapters.CommentAdapter;
@@ -37,6 +46,8 @@ public class ViewEquipmentActivity extends AppCompatActivity {
     private TextView textView_equipmentCategory;
     private TextView textView_noComment;
     private RecyclerView recylerView_commentList;
+    private ImageView imageView_comment;
+    private EditText editText_comment;
     private Equipment equipment = null;
 
     private RequestQueue requestQueue;
@@ -53,7 +64,74 @@ public class ViewEquipmentActivity extends AppCompatActivity {
         textView_equipmentSymbol = findViewById(R.id.activityViewEquipment_textView_equipmentSymbol);
         textView_equipmentCategory = findViewById(R.id.activityViewEquipment_textView_equipmentCategory);
         textView_noComment = findViewById(R.id.activityViewEquipment_textView_noComment);
-        recylerView_commentList = findViewById(R.id.recylerView_commentList);
+        recylerView_commentList = findViewById(R.id.recylerView_commentListEquipment);
+        recylerView_commentList.setLayoutManager(new LinearLayoutManager(this));
+        recylerView_commentList.setHasFixedSize(true);
+        editText_comment = findViewById(R.id.activityViewEquipment_editText_addComment);
+
+        imageView_comment = findViewById(R.id.activityViewEquipment_imageView_addComment);
+
+        imageView_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!MainActivity.isUserLoggedIn(ViewEquipmentActivity.this)) {
+                    ViewEquipmentActivity.this.startActivity(new Intent(ViewEquipmentActivity.this, LoginActivity.class));
+                    Toast.makeText(ViewEquipmentActivity.this, "Please log in to continue", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String comment = editText_comment.getText().toString();
+
+                if(comment.length() == 0) {
+                    editText_comment.setError("Enter your comment!");
+                    return;
+                }
+
+                StringRequest postRequest = new StringRequest(Request.Method.POST, URL_COMMENTS,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                editText_comment.setText("");
+                                editText_comment.clearFocus();
+                                InputMethodManager imm = (InputMethodManager)getSystemService(ViewEquipmentActivity.this.INPUT_METHOD_SERVICE);
+                                imm.hideSoftInputFromWindow(editText_comment.getWindowToken(), 0);
+                                fetchComments();
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // error
+                                Toast.makeText(ViewEquipmentActivity.this, "An error occured posting comment!", Toast.LENGTH_SHORT).show();
+                                error.printStackTrace();
+                            }
+                        }
+                ) {
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = MainActivity.getAuthorizationHeader(ViewEquipmentActivity.this);
+                        return headers != null ? headers : super.getHeaders();
+                    }
+
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("content", comment);
+                        params.put("equipment", equipment.getUrl());
+
+                        return params;
+                    }
+                };
+
+                requestQueue.add(postRequest);
+
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
