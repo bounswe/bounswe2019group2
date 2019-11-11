47b51cc7e03472f1ff768a2af45238a999e690b3
fatih-iver <fatih.iver@boun.edu.tr>
2019-11-11 19:49:52 +0300

    list equipment

---
+++ traiders/mobile/app/src/main/java/tk/traiders/marshallers/EquipmentMarshaller.java
@@ -0,0 +1,62 @@
+package tk.traiders.marshallers;
+
+import android.media.audiofx.DynamicsProcessing;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tk.traiders.constants.ArticleConstants;
+import tk.traiders.constants.EquipmentConstants;
+import tk.traiders.models.Article;
+import tk.traiders.models.Equipment;
+import tk.traiders.models.Like;
+import tk.traiders.models.User;
+import tk.traiders.utils.DateUtils;
+
+public class EquipmentMarshaller {
+
+    public static Equipment unmarshall(String response){
+
+        Equipment equipment = null;
+
+        try {
+
+            JSONObject equipmentAsJson = new JSONObject(response);
+
+            String url = equipmentAsJson.getString(EquipmentConstants.URL);
+            String id = equipmentAsJson.getString(EquipmentConstants.ID);
+            String name = equipmentAsJson.getString(EquipmentConstants.NAME);
+            String category = equipmentAsJson.getString(EquipmentConstants.CATEGORY);
+            String symbol = equipmentAsJson.getString(EquipmentConstants.SYMBOL);
+
+            equipment = new Equipment(url, id, name, category, symbol);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return equipment;
+    }
+
+    public static List<Equipment> unmarshallList(String response) {
+
+        List<Equipment> equipmentList = new ArrayList<>();
+
+        try {
+            JSONArray equipmentListAsJson = new JSONArray(response);
+            for(int i = 0; i < equipmentListAsJson.length(); i++){
+                equipmentList.add(unmarshall(equipmentListAsJson.getString(i)));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return equipmentList;
+
+    }
+}
---
+++ traiders/mobile/app/src/main/java/tk/traiders/ui/social/adapters/EquipmentAdapter.java
@@ -0,0 +1,74 @@
+package tk.traiders.ui.social.adapters;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.TooltipCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import tk.traiders.R;
+import tk.traiders.models.Equipment;
+
+public class EquipmentAdapter extends RecyclerView.Adapter<EquipmentAdapter.EquipmentViewHolder> {
+
+    private Context context;
+    private List<Equipment> equipmentList;
+
+    public EquipmentAdapter(Context context, List<Equipment> equipmentList) {
+        this.context = context;
+        this.equipmentList = equipmentList;
+
+    }
+
+    @NonNull
+    @Override
+    public EquipmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new EquipmentViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.equipment_list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EquipmentViewHolder holder, int position) {
+
+        Equipment equipment = equipmentList.get(position);
+
+        holder.category.setText(equipment.getCategory());
+        holder.name.setText(equipment.getName());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            holder.name.setTooltipText(equipment.getName());
+        } else {
+            TooltipCompat.setTooltipText(holder.name, equipment.getName());
+        }
+        holder.symbol.setText(equipment.getSymbol());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return equipmentList.size();
+    }
+
+    public static class EquipmentViewHolder extends RecyclerView.ViewHolder {
+
+        TextView category;
+        TextView name;
+        TextView symbol;
+
+        public EquipmentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            category = itemView.findViewById(R.id.textView_equipmentCategory);
+            name = itemView.findViewById(R.id.textView_equipmentName);
+            symbol = itemView.findViewById(R.id.textView_equipmentSymbol);
+        }
+    }
+
+
+}
