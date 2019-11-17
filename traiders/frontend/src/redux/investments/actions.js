9a7edd219a5d6e8331d09933be4e5bb3d22d8ad8
Muhammet Furkan Gök <muhammet.gok@boun.edu.tr>
2019-11-17 16:36:50 +0300

    add redux configuration for assets

--- traiders/frontend/src/redux/investments/actions.js
+++ traiders/frontend/src/redux/investments/actions.js
@@ -1,14 +1,16 @@
 import { API } from '../apiConfig';
-import { GetWithAuthorization } from '../../common/http/httpUtil';
+import { GetWithAuthorization, GetWithUrl } from '../../common/http/httpUtil';
 
 /* Action Types */
 
 const SAVE_INVESTMENT_LIST = 'SAVE_INVESTMENT_LIST';
 const SAVE_ASSETS_LIST = 'SAVE_ASSETS_LIST';
+const SAVE_CURRENCY_LIST = 'SAVE_CURRENCY_LIST';
 
 export const actionTypes = {
   SAVE_INVESTMENT_LIST,
-  SAVE_ASSETS_LIST
+  SAVE_ASSETS_LIST,
+  SAVE_CURRENCY_LIST
 };
 
 /* Action Creators */
@@ -27,9 +29,17 @@ function saveAssetList(list) {
   };
 }
 
+function saveCurrencyList(list) {
+  return {
+    tyoe: SAVE_CURRENCY_LIST,
+    payload: list
+  };
+}
+
 export const actionCreators = {
   saveInvestmentList,
-  saveAssetList
+  saveAssetList,
+  saveCurrencyList
 };
 
 /* Api Call Functions */
@@ -51,7 +61,20 @@ export const getAssets = (id, token) => {
   return (dispatch) => {
     GetWithAuthorization(`${API}/asset/?id=${id}`, token)
       .then((response) => response.json())
-      .then((response) => dispatch(saveInvestmentList(response)))
+      .then((response) => dispatch(saveAssetList(response)))
+
+      .catch((error) =>
+        // eslint-disable-next-line no-console
+        console.log('Error while fetching assets\n', error)
+      );
+  };
+};
+
+export const getCurrencyList = () => {
+  return (dispatch) => {
+    GetWithUrl(`${API}/equipment/?category=currency`)
+      .then((response) => response.json())
+      .then((response) => dispatch(saveCurrencyList(response)))
 
       .catch((error) =>
         // eslint-disable-next-line no-console
--- traiders/frontend/src/redux/investments/reducer.js
+++ traiders/frontend/src/redux/investments/reducer.js
@@ -2,7 +2,8 @@ import { actionTypes } from './actions';
 
 const initialState = {
   investments: null,
-  assets: null
+  assets: null,
+  currencyList: null
 };
 
 function investmentReducer(state = initialState, action) {
@@ -16,6 +17,11 @@ function investmentReducer(state = initialState, action) {
       return {
         ...state,
         assets: action.payload
+      };
+    case actionTypes.SAVE_CURRENCY_LIST:
+      return {
+        ...state,
+        currencyList: action.payload
       };
     default:
       return state;
