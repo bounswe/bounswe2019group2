5f907bcf990f964ca4808668ecffe39ea942cd6d
Yunus Emre Inci <yemreinci0@gmail.com>
2019-11-19 18:00:59 +0300

    Allow filter by symbol in equipment comments

--- traiders/backend/api/filters/comment.py
+++ traiders/backend/api/filters/comment.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework as filters
+from django.forms import ValidationError
 
 from ..models import ArticleComment, EquipmentComment, Equipment
 
@@ -12,7 +13,14 @@ class ArticleCommentFilterSet(filters.FilterSet):
 
 
 class EquipmentCommentFilterSet(filters.FilterSet):
-    equipment = filters.ModelChoiceFilter(queryset=Equipment.objects.all())
+    equipment = filters.CharFilter(method='filter_by_equipment')
+
+    def filter_by_equipment(self, queryset, name, value):
+        # value can be either pk or symbol of an equipment
+        try:
+            return queryset.filter(equipment_id=int(value))
+        except ValueError:
+            return queryset.filter(equipment__symbol=value)
 
     class Meta:
         model = EquipmentComment
