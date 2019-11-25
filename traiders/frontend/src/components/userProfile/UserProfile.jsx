8cb4118ef2a93ab3cc6e06d1be60692ce372a07f
Dilruba Köse <dilrubakose@gmail.com>
2019-11-25 23:30:15 +0200

    fix for profile page and follow

--- traiders/frontend/src/components/userProfile/UserProfile.jsx
+++ traiders/frontend/src/components/userProfile/UserProfile.jsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Page from '../page/Page';
+import { API } from '../../redux/apiConfig';
 import UserHeader from '../userHeader/UserHeaderContainer';
 import UserSuccess from '../userSuccess/UserSuccessContainer';
 import './user-profile.scss';
 
-const UserProfile = (props) => {
-  const { user } = props;
-  return (
-    <Page>
-      <div className="profile-container">
-        <div>
-          <UserHeader user={user.user} other={false} />
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      followings: [],
+      followingNumber: 0
+    };
+  }
+
+  componentWillMount() {
+    const { user } = this.props;
+    const array = user.user.url.split('/');
+    const id = array[array.length - 2];
+    fetch(`${API}/following/?user_following=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ followings: data });
+        // eslint-disable-next-line no-console
+        this.setState((prevState) => ({
+          followingNumber: prevState.followings.length
+        }));
+      })
+      // eslint-disable-next-line no-console
+      .catch(console.log);
+  }
+
+  render() {
+    const { user } = this.props;
+    const { followingNumber } = this.state;
+    const array = user.user.url.split('/');
+    const id = array[array.length - 2];
+    return (
+      <Page>
+        <div className="profile-container">
+          <div>
+            <UserHeader
+              userId={id}
+              other={false}
+              followingN={followingNumber}
+              otherUserId={id}
+            />
+          </div>
+          <div>
+            <UserSuccess id={id} />
+          </div>
         </div>
-        <div>
-          <UserSuccess user={user.user} />
-        </div>
-      </div>
-    </Page>
-  );
-};
+      </Page>
+    );
+  }
+}
 
 export default UserProfile;
