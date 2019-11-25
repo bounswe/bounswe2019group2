a0fb702f69dc175f84476983511b8d60a7e15fc9
Muhammet Furkan Gök <muhammet.gok@boun.edu.tr>
2019-11-25 16:04:24 +0300

    add tables for orders

--- traiders/frontend/src/components/myOrders/MyOrders.jsx
+++ traiders/frontend/src/components/myOrders/MyOrders.jsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Table, Button, Modal, Select, Input, Checkbox } from 'antd';
 
 import './my-orders.scss';
-
+import {
+  buyOrderTableColumns,
+  stopLossOrderTableColumns
+} from '../../common/constants/generalConstants';
 import history from '../../common/history';
 import { API } from '../../redux/apiConfig';
 import { PostWithAuthorization } from '../../common/http/httpUtil';
@@ -12,8 +15,29 @@ const { Option } = Select;
 class MyOrders extends Component {
   render() {
     return (
-      <div>
-        <div></div>
+      <div className="order-container">
+        <div className="buy-order-container">
+          <div className="buy-order-table">
+            <Table
+              columns={buyOrderTableColumns}
+              dataSource={newManualInvestmentList}
+              bordered
+              title={() => 'BUY ORDERS'}
+              rowKey="id"
+            />
+          </div>
+        </div>
+        <div className="stop-loss-order-container">
+          <div className="stop-loss-order-table">
+            <Table
+              columns={stopLossOrderTableColumns}
+              dataSource={newManualInvestmentList}
+              bordered
+              title={() => 'MANUAL INVESTMENTS'}
+              rowKey="id"
+            />
+          </div>
+        </div>
       </div>
     );
   }
