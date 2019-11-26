4f237a63ab9ecaa4f9f6f090d66f3a1c29455f84
fatih-iver <fatih.iver@boun.edu.tr>
2019-11-26 06:36:17 +0300

    implement personal articles fragment

--- traiders/mobile/app/src/main/java/tk/traiders/ui/profile/children/PersonalArticlesFragment.java
+++ traiders/mobile/app/src/main/java/tk/traiders/ui/profile/children/PersonalArticlesFragment.java
@@ -8,19 +8,40 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import tk.traiders.MainActivity;
 import tk.traiders.R;
+import tk.traiders.marshallers.ArticleMarshaller;
+import tk.traiders.models.Article;
+import tk.traiders.ui.ListFragment;
+import tk.traiders.ui.profile.adapters.PersonalArticlesAdapter;
 
 
-public class PersonalArticlesFragment extends Fragment {
+public class PersonalArticlesFragment extends ListFragment {
 
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        View rootView = inflater.inflate(R.layout.fragment_personal_articles, container, false);
-        return rootView;
+    protected String getURL() {
+        return "https://api.traiders.tk/articles/";
     }
 
+    @Override
+    protected RecyclerView.Adapter getAdapter(String response) {
+        List<Article> articleList = ArticleMarshaller.unmarshallList(response);
+        String userId = MainActivity.getUserID(getContext());
 
+        List<Article> filteredArticles = new ArrayList<>();
+
+        for(Article article: articleList) {
+            if(article.getAuthor().getId().equals(userId)){
+                filteredArticles.add(article);
+            }
+        }
+
+        return new PersonalArticlesAdapter(getContext(), filteredArticles);
+    }
 }
