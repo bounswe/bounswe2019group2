c630c4059444e2afcae2cf341c9c95c012d78921
fatih-iver <fatih.iver@boun.edu.tr>
2019-11-26 08:06:26 +0300

    comment like & unlike

--- traiders/mobile/app/src/main/java/tk/traiders/components/article/ViewArticleActivity.java
+++ traiders/mobile/app/src/main/java/tk/traiders/components/article/ViewArticleActivity.java
@@ -200,7 +200,13 @@ public class ViewArticleActivity extends AppCompatActivity {
                 Toast.makeText(ViewArticleActivity.this, "An error occured fetching comments", Toast.LENGTH_SHORT).show();
                 error.printStackTrace();
             }
-        });
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = MainActivity.getAuthorizationHeader(ViewArticleActivity.this);
+                return headers != null ? headers : super.getHeaders();
+            }
+        };
 
         requestQueue.add(request);
 
