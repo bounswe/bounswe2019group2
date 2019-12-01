ccb43e31696b3d3a2de97f04024104d0ac5a6731
Yunus Emre Inci <yemreinci0@gmail.com>
2019-12-01 18:46:05 +0300

    Remove irrelevant files

--- traiders/backend/api/serializers/__init__.py
+++ traiders/backend/api/serializers/__init__.py
@@ -10,4 +10,3 @@ from .event import EventSerializer
 from .prediction import PredictionSerializer
 from .investment import ManualInvestmentSerializer, AssetSerializer, OnlineInvestmentSerializer
 from .order import BuyOrderSerializer, StopLossOrderSerializer
-from .portfolio import PortfolioSerializer
