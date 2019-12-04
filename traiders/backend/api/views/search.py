7a9bc7bfe55d2e6e4f6f6eacc5f3581122c6fddb
mertyg <mertyuksekgonul@gmail.com>
2019-12-04 15:01:42 +0300

    search with datamuse

--- traiders/backend/api/views/search.py
+++ traiders/backend/api/views/search.py
@@ -5,11 +5,16 @@ from django.db.models import Q
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import serializers
+import requests as rq
+import logging
 
 from ..models import *
 from ..serializers import *
 
 MAX_ITEMS = 5
+logger = logging.getLogger(__name__)
+
+
 class SearchViewSet(GenericViewSet):
     """
     View searching a given string
@@ -24,16 +29,29 @@ class SearchViewSet(GenericViewSet):
         user = UserSerializer()
 
     @staticmethod
-    def find_closest(keyword, k=1):
-        if k == 1:
-            kws = [(1, keyword)]
+    def find_closest(keyword, top_k=10):
+        words = keyword.split(" ")
+        kws = []
+        for word in words:
+            kws.append((1, word))
+
+        res = rq.get(f'https://api.datamuse.com/words?ml={keyword}')
+        similars = res.json()
+
+        if res.status_code != 200:
+            logger.log("Datamuse returned non-200")
             return kws
+
+        if similars != []:
+            max_score = similars[0]['score']
+            for dct in similars[:top_k]:
+                kws.append((dct['score']/max_score, dct['word']))
+        return kws
+
 
     @staticmethod
     def iterate_and_add(kw_objects, serializer, context):
-        objects = []
-        for i, object in enumerate(kw_objects):
-            objects.append(serializer(object, context=context).data)
+        objects = serializer(kw_objects, many=True, context=context).data
         return objects
 
     def list(self, request):
@@ -45,28 +63,35 @@ class SearchViewSet(GenericViewSet):
             }, status=status.HTTP_400_BAD_REQUEST)
 
         context = self.get_serializer_context()
+
         keywords = self.find_closest(params["keyword"])
 
         # Sort by relevance
         keywords = sorted(keywords, key=lambda x: x[0])
+        articles = list()
+        events = list()
+        parities = list()
+        equipments = list()
+        users = list()
 
         for kw in keywords:
-            articles = self.iterate_and_add(Article.objects.filter(content__contains=kw[1])[:MAX_ITEMS],
-                                            ArticleSerializer,
-                                            context)
-            events = self.iterate_and_add(Event.objects.filter(event__contains=kw[1])[:MAX_ITEMS],
-                                          EventSerializer,
-                                          context)
-            parities = self.iterate_and_add(Parity.objects.filter(Q(base_equipment__name__contains=kw[1]) |
-                                                                  Q(target_equipment__name__contains=kw[1]))[:MAX_ITEMS],
-                                            ParitySerializer,
-                                            context)
-            equipments = self.iterate_and_add(Equipment.objects.filter(name__contains=kw[1])[:MAX_ITEMS],
-                                              EquipmentSerializer,
-                                              context)
-            users = self.iterate_and_add(User.objects.filter(username__contains=kw[1])[:MAX_ITEMS],
-                                         UserSerializer,
-                                         context)
+            articles.extend(self.iterate_and_add(Article.objects.filter(content__contains=kw[1])[:MAX_ITEMS],
+                                                 ArticleSerializer,
+                                                 context))
+            events.extend(self.iterate_and_add(Event.objects.filter(event__contains=kw[1])[:MAX_ITEMS],
+                                               EventSerializer,
+                                               context))
+            parities.extend(self.iterate_and_add(Parity.objects.filter(Q(base_equipment__name__contains=kw[1]) |
+                                                 Q(target_equipment__name__contains=kw[1]))[:MAX_ITEMS],
+                                                 ParitySerializer,
+                                                 context))
+            equipments.extend(self.iterate_and_add(Equipment.objects.filter(name__contains=kw[1])[:MAX_ITEMS],
+                                                   EquipmentSerializer,
+                                                   context))
+            users.extend(self.iterate_and_add(User.objects.filter(username__contains=kw[1])[:MAX_ITEMS],
+                                              UserSerializer,
+                                              context))
+
         all_objects = dict()
         all_objects["articles"] = articles
         all_objects["events"] = events
