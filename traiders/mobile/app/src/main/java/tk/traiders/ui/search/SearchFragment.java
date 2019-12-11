a70dead45deb01df328d934ef40300c351f01ca8
fatih-iver <fatih.iver@boun.edu.tr>
2019-12-11 21:30:47 +0300

    implement search & recommendations

---
+++ traiders/mobile/app/src/main/java/tk/traiders/ui/search/SearchFragment.java
@@ -0,0 +1,35 @@
+package tk.traiders.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import tk.traiders.R;
+import tk.traiders.ui.portfolio.PortfolioViewPagerAdapter;
+
+public class SearchFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ViewPager viewPager = view.findViewById(R.id.viewPager_search);
+        viewPager.setAdapter(new SearchViewPagerAdapter(getChildFragmentManager()));
+
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout_search);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
+
---
+++ traiders/mobile/app/src/main/java/tk/traiders/ui/search/SearchViewPagerAdapter.java
@@ -0,0 +1,47 @@
+package tk.traiders.ui.search;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import tk.traiders.ui.portfolio.children.AlarmsFragment;
+import tk.traiders.ui.portfolio.children.ChildPortfolioFragment;
+import tk.traiders.ui.search.children.RecommendationsFragment;
+import tk.traiders.ui.search.children.SubSearchFragment;
+
+public class SearchViewPagerAdapter extends FragmentPagerAdapter {
+
+    public SearchViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new SubSearchFragment();
+            case 1:
+                return new RecommendationsFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Search";
+            case 1:
+                return "Recommendations";
+        }
+        return super.getPageTitle(position);
+    }
+}
