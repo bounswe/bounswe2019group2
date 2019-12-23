6b923909a3fa6053c929601355016d042d397091
alperenkozkan <kursat.ozkan@boun.edu.tr>
2019-12-23 20:31:39 +0300

    Total profit has been added to investment page and bugfix related to sell/buy base/target equipment on manual investment and online investment mismatch is fixed.

--- traiders/frontend/src/components/myInvestments/MyInvestments.jsx
+++ traiders/frontend/src/components/myInvestments/MyInvestments.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Button, Modal, Select, Input, Checkbox } from 'antd';
+import { Table, Button, Modal, Select, Input, Checkbox, Alert } from 'antd';
 
 import history from '../../common/history';
 import './my-investments.scss';
@@ -212,6 +212,7 @@ class MyInvestments extends Component {
       creditCardValidUntil,
       isCreditCard
     } = this.state;
+
     let newManualInvestmentList = [];
     let newOnlineInvestmentList = [];
     if (user && manualInvestments && onlineInvestments && profitLossList) {
@@ -345,7 +346,7 @@ class MyInvestments extends Component {
               <Select
                 defaultValue="TRY"
                 onChange={(value) =>
-                  this.handleSelectCurrency('onlineBaseCurrency', value)
+                  this.handleSelectCurrency('onlineTargetCurrency', value)
                 }
                 style={{ width: 120 }}
               >
@@ -358,7 +359,7 @@ class MyInvestments extends Component {
               <Select
                 defaultValue="TRY"
                 onChange={(value) =>
-                  this.handleSelectCurrency('onlineTargetCurrency', value)
+                  this.handleSelectCurrency('onlineBaseCurrency', value)
                 }
                 style={{ width: 120 }}
               >
@@ -403,6 +404,17 @@ class MyInvestments extends Component {
             )}
           </div>
         </Modal>
+        {profitLossList && profitLossList > 0 ? (
+          <div>
+            Total Profit:
+            <Alert message={profitLossList.total_profit} type="success" />
+          </div>
+        ) : (
+          <div>
+            Total Profit:
+            <Alert message={profitLossList.total_profit} type="error" />
+          </div>
+        )}
       </div>
     );
   }
