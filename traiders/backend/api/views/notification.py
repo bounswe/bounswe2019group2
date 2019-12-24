41e45f0223803237d943fae6c2037237552c44c9
Yunus Emre Inci <yemreinci0@gmail.com>
2019-12-24 19:14:59 +0300

    Ignore deleted

--- traiders/backend/api/views/notification.py
+++ traiders/backend/api/views/notification.py
@@ -1,10 +1,11 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet
-from ..models import Notification
+from ..models import Notification, Event, OnlineInvestment, Article
 from ..serializers import NotificationSerializer
 from rest_framework.exceptions import PermissionDenied
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class NotificationViewSet(mixins.RetrieveModelMixin,
@@ -22,3 +23,25 @@ class NotificationViewSet(mixins.RetrieveModelMixin,
         # Another user cannot
         if request.user != notification.user:
             raise PermissionDenied
+
+    def list(self, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        notifs = []
+
+        for notif in queryset:
+            print(notif.reference_url)
+            _, type, id, _ = notif.reference_url.split('/')
+
+            model = None
+            if type == 'events':
+                model = Event
+            elif type == 'articles':
+                model = Article
+            elif type == 'onlineinvestment':
+                model = OnlineInvestment
+
+            if model and model.objects.filter(id=int(id)).exists():
+                notifs.append(notif)
+
+        serializer = self.get_serializer(notifs, many=True)
+        return Response(serializer.data)
